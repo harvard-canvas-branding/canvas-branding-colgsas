b2f4af7ab1ad43e6c5559c13dae243cd379505ab
Arthur Barrett <abarrett@fas.harvard.edu>
2020-11-16 13:00:52 -0500

    Updated text and styling based on feedback.

--- js/course_settings_import.js
+++ js/course_settings_import.js
@@ -12,13 +12,14 @@
     function show_import_notification() {
         var content = document.querySelector("#content");
         var notification = document.createElement("div");
-        notification.className = "ic-notification";
+        notification.className = "ic-notification ic-notification--alert";
         notification.innerHTML =
             '<div class="ic-notification__icon" role="presentation">' +
                 '<i class="icon-info"></i>' +
-                '<span class="screenreader-only">information</span>' +
+                '<span class="screenreader-only">alert</span>' +
             '</div>' +
             '<div style="width:100%;padding:.5em;">' +
+                '<b>Attention Spring 2020 Courses:&nbsp;</b>' +
                 'Use "Select specific content" to exclude Calendar Events when copying from previous courses. Events will include old Zoom links that will not work in your new course. <a href="https://harvard.service-now.com/ithelp?id=kb_article&sys_id=8b1898dcdba0a810babda8dad39619bd" target="_blank">Learn how to exclude these items</a> when importing your course content.' +
             '</div>';
         content.insertBefore(notification, content.firstChild);
